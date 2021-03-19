8ff58ba095363be0ab7c36b81a5950ab23e62f12
ShivSeth <shivam.prakash114@gmail.com>
2021-03-19 15:31:43 +0530

    backend done

--- backend/server.js
+++ backend/server.js
@@ -21,10 +21,16 @@ connection.once("open", () => {
 app.use(cors());
 app.use(express.json());
 
+const exercisesRouter = require("./routes/exercises");
+const usersRouter = require("./routes/users");
+
+app.use("/exercises", exercisesRouter);
+app.use("/users", usersRouter);
+
 app.listen(port, () => {
   console.log(`Server is running on Port: ${port}`);
 });
 
 app.get("/", (req, res) => {
-  res.send("Welcome");
+  res.send("Richa Prakash");
 });
