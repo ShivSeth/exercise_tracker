faa4a0c45157567a37c7a9b490265472b9b898fe
ShivSeth <shivam.prakash114@gmail.com>
2021-03-19 18:07:29 +0530

    almost done

---
+++ src/components/edit-exercise.component.js
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+
+export default class EditExercise extends Component {
+  render() {
+    return (
+      <div>
+        <h1>You are on Edit Exercise Page</h1>
+      </div>
+    );
+  }
+}
---
+++ src/components/exercises-list.component.js
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+
+export default class ExercisesList extends Component {
+  render() {
+    return (
+      <div>
+        <h1>You are on Exercises List page</h1>
+      </div>
+    );
+  }
+}
